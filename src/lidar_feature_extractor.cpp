26997fd4adf928bd6a629006281f83b95123a3f1
libing64 <libing64@gmail.com>
2020-11-21 18:18:00 +0800

    project to depth image

--- src/lidar_feature_extractor.cpp
+++ src/lidar_feature_extractor.cpp
@@ -56,7 +56,7 @@ void velodyne_points_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
     ROS_INFO_STREAM("line: " << __LINE__);
     pcl::fromROSMsg(*msg, *pc_raw);
 
-    return;
+    //return;
     ROS_INFO_STREAM("line: " << __LINE__);
     std::vector<int> index;
     pcl::removeNaNFromPointCloud(*pc_raw, *pc_raw, index);
@@ -66,6 +66,8 @@ void velodyne_points_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
     int rows = channels;
     ROS_INFO_STREAM("line: " << __LINE__);
     cv::Mat depth = cv::Mat(rows, cols, CV_32F, cv::Scalar::all(0));
+    imshow("pc_raw", depth);
+    cv::waitKey(2);
     ROS_INFO_STREAM("line: " << __LINE__);
     //project pc_raw to depth image
     for (int i = 0; i < pc_raw->size(); i++)
@@ -78,11 +80,11 @@ void velodyne_points_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
         //64 * 2000
         int row = round((elevation - vfov_min) / vres);
         int col = round(azimuth / (2 * M_PI) * cols);
-        depth.at<float>(row, col) = p.z;
+        depth.at<float>(row, col) = sqrtf(hori_dist * hori_dist + p.z * p.z);
     }
     ROS_INFO_STREAM("line: " << __LINE__);
-    imshow("pc_raw", depth);
-    cv::waitKey(2);
+    // imshow("pc_raw", depth);
+    // cv::waitKey(2);
 
 }
 
